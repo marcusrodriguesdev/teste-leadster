c1f390f84f0fc1e546f4889b075e625de59541fc
marcusrodriguesdev <marcus.rodrigues10@hotmail.com>
2021-11-25 13:39:47 -0300

    create header

---
+++ src/components/Header/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+function Header() {
+  return (
+    <header>
+      Album de fotos
+    </header>
+  );
+}
+
+export default Header;
