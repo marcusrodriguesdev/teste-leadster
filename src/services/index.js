8a3fd051af659fedda6222e890a2d1f52c142b8c
marcusrodriguesdev <marcus.rodrigues10@hotmail.com>
2021-11-25 13:06:52 -0300

    request api pexels

---
+++ src/services/index.js
@@ -0,0 +1,13 @@
+const AUTHORIZATION_KEY = '563492ad6f91700001000001ba630320db23455a92fa3fafe7edaf59';
+const API_PEXELS = 'https://api.pexels.com/v1/curated';
+
+async function requestApiPexels() {
+  const requiriment = await fetch(API_PEXELS, {
+    headers: {
+      Authorization: AUTHORIZATION_KEY,
+    },
+  }).then((response) => response.json());
+  return requiriment;
+}
+
+export default requestApiPexels;
