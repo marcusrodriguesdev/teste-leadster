17657f028c516d31ea55567b117f495e58aad90d
marcusrodriguesdev <marcus.rodrigues10@hotmail.com>
2021-12-01 11:21:56 -0300

    finish theme dark and light

---
+++ src/styles/dark.js
@@ -0,0 +1,8 @@
+export default {
+  title: 'dark',
+  colors: {
+    primary: '#212529',
+    secondary: '#d7d7d7',
+    text: '#f7fafc',
+  },
+};
---
+++ src/styles/light.js
@@ -0,0 +1,8 @@
+export default {
+  title: 'light',
+  colors: {
+    primary: '#fff',
+    secondary: '#d7d7d7',
+    text: '#212529',
+  },
+};
