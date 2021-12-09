0b2e9737f617e6efa3e8b2babd114872afd7fff1
marcusrodriguesdev <marcus.rodrigues10@hotmail.com>
2021-12-09 15:25:02 -0300

    change url api

--- src/services/index.js
+++ src/services/index.js
@@ -1,9 +1,9 @@
 const AUTHORIZATION_KEY = '563492ad6f91700001000001ba630320db23455a92fa3fafe7edaf59';
-const API_PEXELS = 'https://api.pexels.com/v1/curated?per_page=32';
+const API_PEXELS = (page) => `https://api.pexels.com/v1/curated?page=${page}&per_page=16`;
 
-async function requestApiPexels() {
+async function requestApiPexels(currentPage) {
   const requiriment = await
-  fetch(API_PEXELS, {
+  fetch(API_PEXELS(currentPage), {
     headers: {
       Authorization: AUTHORIZATION_KEY,
     },
